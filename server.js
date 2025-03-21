eedd56ba3aaf5d7af3c20c8ba63f46c697c86c5a
alrigo123 <alexestrada2804@gmail.com>
2025-03-21 14:51:20 -0500

    port

--- server.js
+++ server.js
@@ -36,5 +36,5 @@ io.on("connection", (socket) => {
     });
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Servidor WebSocket corriendo en el puerto ${PORT}`));
